42257b23c4a105ae439e723f1706fb820c789a4a
aronmandos <aronmandos@live.co.uk>
2024-12-04 13:42:31 +0100

    init files Day4

---
+++ Day4/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Day4;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("# Day 3");
+        DayFourPartOne(true);
+        Console.WriteLine();
+        DayFourPartOne(false);
+        //
+        // Console.WriteLine();
+        // DayFourPartTwo(true);
+        // Console.WriteLine();
+        // DayFourPartTwo(false);
+    }
+
+    private static void DayFourPartOne(bool testInput)
+    {
+        Console.WriteLine();
+        var testString = testInput ? "test" : "";
+        Console.WriteLine($"## Day 4 Part 1 {testString}");
+        Console.WriteLine();
+
+        var fileName = testInput ? "testinput.txt" : "input.txt";
+        using var reader = File.OpenText(fileName);
+        //var fullText = reader.ReadToEnd();
+    }
+
+    private static void DayFourPartTwo(bool testInput)
+    {
+        Console.WriteLine();
+        var testString = testInput ? "test" : "";
+        Console.WriteLine($"## Day 4 Part 2 {testString}");
+        Console.WriteLine();
+
+        var fileName = testInput ? "testinput.txt" : "input.txt";
+        using var reader = File.OpenText(fileName);
+        //var fullText = reader.ReadToEnd();
+    }
+}

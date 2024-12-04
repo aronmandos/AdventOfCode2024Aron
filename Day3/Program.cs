cd49446966fb4859bbbbd643ca0fb826cee870c9
aronmandos <aronmandos@live.co.uk>
2024-12-04 11:01:51 +0100

    Added init of Day3

---
+++ Day3/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Day3;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("# Day 3");
+        Console.WriteLine("test:");
+        DayThreePartOne(true);
+        Console.WriteLine();
+        DayThreePartOne(false);
+    }
+
+    private static void DayThreePartOne(bool testinput)
+    {
+        
+        Console.WriteLine();
+        var teststring = testinput ? "test" : "";
+        Console.WriteLine($"## Day 3 Part One {teststring}");
+        Console.WriteLine();
+        
+        var fileName = testinput ? "testinput.txt" : "input.txt";
+        using var reader = File.OpenText(fileName);
+
+    }
+}
